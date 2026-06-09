415d31794a5ebdcd60af82bc735b270678c37abe
talhatanoli078-ui <talhatanoli078@gmail.com>
2026-06-09 10:02:37 +0500

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ st.markdown("""
 # ---------------- LOAD DATA ----------------
 @st.cache_data
 def load_data():
-    return pd.read_csv("StudentsPerformance (2).csv")
+    pd.read_csv("studentsperformance.csv")
 
 df = load_data()
 
